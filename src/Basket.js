fe5de59e817bfdc3ac26a1f9fb3a763a00b5028b
zeeshan191 <80030867+zeeshan191@users.noreply.github.com>
2021-08-27 16:19:04 +0530

    gold

--- src/Basket.js
+++ src/Basket.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { FaPlus,FaMinus } from 'react-icons/fa';
 
 export default function Basket(props) {
   const { cartItems, onAdd, onRemove } = props;
   const itemsPrice = cartItems.reduce((a, c) => a + c.qty * c.price, 0);
   const taxPrice = itemsPrice * 0.14;
-  const shippingPrice = itemsPrice > 2000 ? 0 : 20;
-  const totalPrice = itemsPrice + taxPrice + shippingPrice;
+  
+  const totalPrice = itemsPrice + taxPrice ;
   return (
     <aside className="block col-12" >
       <h2 style={{marginTop:'40px',color:'blue'}}>Your Selected Items</h2>
@@ -15,12 +16,12 @@ export default function Basket(props) {
           <div key={item.id} className="row">
             <div className="col-6">{item.name}</div>
             <div className="plus1">
-              <button onClick={() => onRemove(item)} className="remove">
-                -
-              </button>{' '}
-              <button onClick={() => onAdd(item)} className="add">
-                +
-              </button>
+             
+           <button onClick={() => onRemove(item)}>   <FaMinus  />
+           </button>
+             <button  onClick={() => onAdd(item)}><FaPlus /></button>
+              
+          
             </div>
 
             <div className=" text-right" >
@@ -40,19 +41,13 @@ export default function Basket(props) {
               <div className="item1">Tax Price</div>
               <div className="text-right">Rs{taxPrice.toFixed(2)}</div>
             </div>
-            <div className="row">
-              <div className="item1">Shipping Price</div>
-              <div className=" text-right">
-                {shippingPrice.toFixed(2)}
-              </div>
-            </div>
-
+            
             <div className="row">
               <div className="item1">
                 <strong>Total Price</strong>
               </div>
               <div className=" text-right">
-                <strong>${totalPrice.toFixed(2)}</strong>
+                <strong>Rs{totalPrice.toFixed(2)}</strong>
               </div>
             </div>
             <hr />
